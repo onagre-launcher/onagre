58adab48ddba7a667de5659c70b083bf6ccfe351
Paul Delafosse <paul.delafosse@protonmail.com>
2020-11-03 20:35:27 +0100

    feat: search and select

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,9 @@ use iced::futures::StreamExt;
 use crate::desktop::DesktopEntry;
 use fuzzy_matcher::skim::SkimMatcherV2;
 
-use iced::{
-    button, scrollable, text_input, Align, Application, Button, Checkbox,
-    Column, Command, Container, Element, Font, HorizontalAlignment, Length,
-    Row, Scrollable, Settings, Text, TextInput,
-};
+use iced::{button, scrollable, text_input, Align, Application, Rule, Button, Checkbox, Column, Command, Container, Element, Font, HorizontalAlignment, Length, Row, Scrollable, Settings, Text, TextInput, Subscription, Color};
 use fuzzy_matcher::FuzzyMatcher;
+use iced_native::{Widget, Event};
 
 fn main() -> iced::Result {
     Onagre::run(Settings::default())
@@ -26,11 +23,12 @@ fn main() -> iced::Result {
 #[derive(Debug)]
 struct Onagre {
     desktop_entries: Vec<String>,
-    state: State
+    state: State,
 }
 
 #[derive(Debug, Default)]
 struct State {
+    selected: usize,
     scroll: scrollable::State,
     input: text_input::State,
     input_value: String,
@@ -40,6 +38,7 @@ struct State {
 #[derive(Debug, Clone)]
 enum Message {
     InputChanged(String),
+    EventOccurred(iced_native::Event),
 }
 
 impl Application for Onagre {
@@ -54,6 +53,7 @@ impl Application for Onagre {
             .collect();
 
         let state = State {
+            selected: 0,
             scroll: Default::default(),
             input: Default::default(),
             input_value: "".to_string(),
@@ -70,7 +70,12 @@ impl Application for Onagre {
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
             Message::InputChanged(input) => {
+                if self.state.input_value != input {
+                    self.state.selected = 0;
+                }
+
                 self.state.input_value = input;
+
                 if self.state.input_value == "" {
                     self.state.matches = self.desktop_entries.clone()
                 } else {
@@ -79,9 +84,11 @@ impl Application for Onagre {
 
                 Command::none()
             }
-            _ => Command::none()
+            Message::EventOccurred(event) => {
+                self.handle_hotkey(event);
+                Command::none()
+            }
         }
-
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
@@ -105,16 +112,70 @@ impl Application for Onagre {
             .push(title)
             .push(input);
 
-        Scrollable::new(&mut self.state.scroll)
+
+        /* let rows: Vec<Row<Message>> = self.state.matches.iter()
+             .map(|text| Row::new().push(Text::new(text)))
+             .collect();*/
+
+        // let mut text = vec![];
+
+       // let rows = Row::with_children(text)
+       //     .height(Length::Fill);
+
+
+        let mut scrollable = Scrollable::new(&mut self.state.scroll)
             .padding(40)
-            .push(Container::new(content).width(Length::Fill).center_x())
-            .push(Text::new(
-                self.state.matches.join("\n"),
-            ))
-            .into()
+            .push(Container::new(content));
+
+
+        for (idx, entry) in self.state.matches.iter().enumerate() {
+
+            let color = if idx == self.state.selected {
+                [0.0, 1.0, 0.0]
+            } else {
+                [0.5, 0.5, 0.5]
+            };
+
+            scrollable = scrollable.push(Row::new()
+                .push(Text::new(entry).color(color).width(Length::Shrink))
+
+            );
+        }
+
+        scrollable.into()
+
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        iced_native::subscription::events().map(Message::EventOccurred)
     }
 }
 
+
+impl Onagre {
+    fn handle_hotkey(&mut self, event: iced_native::Event) -> Option<Message> {
+        use iced_native::keyboard::KeyCode;
+
+        match event {
+            Event::Keyboard(keyboard_event) => {
+                match keyboard_event {
+                    iced_native::keyboard::Event::KeyPressed { key_code, modifiers } => {
+                        match key_code {
+                            KeyCode::Up => if self.state.selected != 0  { self.state.selected -=1 },
+                            KeyCode::Down => if self.state.selected != self.state.matches.len() - 1 { self.state.selected += 1},
+                            _ => {}
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+
+        None
+    }
+
+}
 impl Onagre {
     fn search(&self, input: &str) -> Vec<String> {
         let matcher = SkimMatcherV2::default();
