e1c45c0cd451346b35d76d83e0520705f4395a57
Paul Delafosse <paul.delafosse@protonmail.com>
2021-10-13 15:18:51 +0200

    refactor: use structop instead of clap

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ extern crate log;
 #[macro_use]
 extern crate anyhow;
 
-use clap::{App, Arg};
-
 mod app;
 mod config;
 mod db;
@@ -20,6 +18,7 @@ use crate::style::theme::Theme;
 use freedesktop::IconFinder;
 use std::path::PathBuf;
 use std::sync::Mutex;
+use structopt::StructOpt;
 
 lazy_static! {
     pub static ref SETTINGS_PATH: Mutex<PathBuf> = {
@@ -38,7 +37,9 @@ lazy_static! {
 
         Mutex::new(path)
     };
+
     pub static ref THEME: Theme = Theme::load();
+
     pub static ref SETTINGS: OnagreSettings = {
         match OnagreSettings::get() {
             Err(err) => {
@@ -57,43 +58,46 @@ lazy_static! {
     };
 }
 
+#[derive(StructOpt)]
+#[structopt(name = "onagre", author = "Paul D. <paul.delafosse@protonmail.com>")]
+struct Cli {
+    #[structopt(
+        name = "config",
+        short = "c",
+        long = "config",
+        takes_value = true,
+        help = "Path to an alternate onagre config file"
+    )]
+    config: Option<PathBuf>,
+
+    #[structopt(
+        name = "theme",
+        short = "t",
+        long = "theme",
+        takes_value = true,
+        help = "Path to an alternate onagre theme file"
+    )]
+    theme: Option<PathBuf>,
+}
+
+
+
 pub fn main() -> iced::Result {
     env_logger::init();
+    let cli = Cli::from_args();
+    // User defined theme config, $XDG_CONFIG_DIR/onagre/theme.toml otherwise
+    if let Some(theme_path) = cli.theme {
+        let path = theme_path.canonicalize();
+        if let Ok(path) = path {
+            *THEME_PATH.lock().unwrap() = path;
+        }
 
-    let default_config_path = dirs::config_dir().unwrap().join("onagre");
-    let default_settings = default_config_path.join("config.toml");
-    let default_settings = default_settings.to_str().unwrap();
-
-    let default_theme = default_config_path.join("theme.toml");
-    let default_theme = default_theme.to_str().unwrap();
-
-    let matches = App::new("Onagre")
-        .setting(clap::AppSettings::ColorAuto)
-        .setting(clap::AppSettings::ColoredHelp)
-        .version(env!("CARGO_PKG_VERSION"))
-        .author("Paul Delafosse <paul.delafosse@protonmail.com>")
-        .about("A general purpose app launcher for wayland and X")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .takes_value(true)
-                .help("Path to an alternate onagre config file")
-                .default_value(default_settings),
-        )
-        .arg(
-            Arg::with_name("theme")
-                .short("t")
-                .long("theme")
-                .takes_value(true)
-                .help("Path to an alternate onagre theme file")
-                .default_value(default_theme),
-        )
-        .get_matches();
+        debug!("Using alternate theme : {:?}", THEME_PATH.lock().unwrap());
+    }
 
     // User defined config, $XDG_CONFIG_DIR/onagre/theme.toml otherwise
-    if let Some(config_path) = matches.value_of("config") {
-        let path = PathBuf::from(config_path).canonicalize();
+    if let Some(config_path) = cli.config {
+        let path = config_path.canonicalize();
         if let Ok(path) = path {
             *SETTINGS_PATH.lock().unwrap() = path;
         }
@@ -102,16 +106,6 @@ pub fn main() -> iced::Result {
             "Using alternate config : {:?}",
             SETTINGS_PATH.lock().unwrap()
         );
-    }
-
-    // User defined theme config, $XDG_CONFIG_DIR/onagre/theme.toml otherwise
-    if let Some(theme_path) = matches.value_of("theme") {
-        let path = PathBuf::from(theme_path).canonicalize();
-        if let Ok(path) = path {
-            *THEME_PATH.lock().unwrap() = path;
-        }
-
-        debug!("Using alternate theme : {:?}", THEME_PATH.lock().unwrap());
     }
 
     app::run()
