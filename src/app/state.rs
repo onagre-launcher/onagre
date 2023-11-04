cee8dadb76e0fc25c568e75ddb6669d5b74135c8
Juan Pablo <juanolon@gmail.com>
2023-11-05 00:26:29 +0100

    feat: launch onagre with mode as cli parameter

--- src/app/state.rs
+++ src/app/state.rs
@@ -21,12 +21,44 @@ pub struct State<'a> {
     pub plugin_matchers: PluginConfigCache,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct PluginConfigCache {
     pub(crate) inner: HashMap<String, Plugin>,
 }
 
 impl PluginConfigCache {
+    pub fn load() -> Self {
+        let mut cache = HashMap::new();
+        for (path, config, regex) in onagre_launcher_toolkit::service::load::from_paths() {
+            let icon: Option<IconPath> = THEME.icon_theme.as_ref().and_then(|theme| {
+                config
+                    .icon
+                    .as_ref()
+                    .map(|source| (source, theme))
+                    .and_then(|(source, theme)| IconPath::from_source(source, theme))
+            });
+
+            let name = path
+                .parent()
+                .expect("Plugin config should have a parent directory")
+                .file_name()
+                .expect("Plugin directory should have an utf8 filename")
+                .to_string_lossy()
+                .to_string();
+
+            let plugin = Plugin {
+                name: name.clone(),
+                icon,
+                history: config.history,
+                help: config.query.help.map(|h| h.to_string()),
+                regex,
+            };
+
+            cache.insert(name, plugin);
+        }
+
+        PluginConfigCache { inner: cache }
+    }
     pub fn get_plugin_icon(&self, plugin_name: &str) -> Option<IconPath> {
         self.inner.get(plugin_name).and_then(|de| de.icon.clone())
     }
@@ -62,52 +94,51 @@ impl State<'_> {
         }
     }
 
-    // TODO: refactor me please
+    pub fn with_mode(mode_query: &str) -> Self {
+        let plugin_matchers = PluginConfigCache::load();
+        let plugin_split = match_web_plugins(mode_query).or_else(|| {
+            plugin_matchers
+                .inner
+                .values()
+                .map(|matcher| matcher.try_match(mode_query))
+                .find_map(|match_| match_)
+        });
+
+        let mode = plugin_split
+            .as_ref()
+            .map(|split| ActiveMode::from(split.clone()))
+            .unwrap_or_default();
+        let modifier_display = plugin_split
+            .as_ref()
+            .map(|query_data| query_data.modifier.clone())
+            .unwrap_or_default();
+        let input_display = plugin_split
+            .map(|query_data| query_data.query)
+            .unwrap_or_default();
+
+        State {
+            selected: Selection::History(0),
+            cache: Default::default(),
+            pop_search: Default::default(),
+            scroll: Default::default(),
+            input_value: SearchInput {
+                mode,
+                modifier_display,                  // dgg
+                input_display,                     // <search str>
+                pop_query: mode_query.to_string(), // dgg <search str>
+            },
+            exec_on_next_search: false,
+            plugin_matchers,
+        }
+    }
+
     pub fn set_input(&mut self, input: &str) {
         let previous_modi = self.input_value.modifier_display.clone();
 
         if !previous_modi.is_empty() {
-            if input.is_empty() {
-                self.input_value.modifier_display = "".to_string();
-                self.input_value.input_display = if THEME.plugin_hint().is_none() {
-                    input.to_string()
-                } else {
-                    previous_modi
-                };
-                self.input_value.mode = ActiveMode::DesktopEntry;
-                let _: iced::Command<Message> = text_input::move_cursor_to_end(INPUT_ID.clone());
-            } else {
-                self.input_value.input_display = input.to_string();
-            }
+            self.set_input_with_modifier(input, previous_modi);
         } else {
-            let terms = &format!("{}{}", previous_modi, input);
-            let plugin_split = match_web_plugins(terms).or_else(|| {
-                self.plugin_matchers
-                    .inner
-                    .values()
-                    .map(|matcher| matcher.try_match(terms))
-                    .find_map(|match_| match_)
-            });
-
-            if let Some(query_data) = plugin_split {
-                self.input_value.modifier_display = query_data.modifier.clone();
-                self.input_value.mode = ActiveMode::from(query_data.clone());
-                // If plugin-hint is disabled use the full input,
-                // otherwise use the split value
-                self.input_value.input_display = if THEME.plugin_hint().is_none() {
-                    input.to_string()
-                } else {
-                    query_data.query
-                };
-            } else {
-                self.input_value.input_display = input.to_string();
-
-                if input.is_empty() {
-                    self.input_value.mode = ActiveMode::History
-                } else {
-                    self.input_value.mode = ActiveMode::DesktopEntry
-                }
-            }
+            self.set_input_without_modifier(input);
         };
 
         let pop_query = match &self.input_value.mode {
@@ -128,6 +159,51 @@ impl State<'_> {
             self.input_value.mode, self.input_value.input_display
         );
     }
+
+    fn set_input_without_modifier(&mut self, input: &str) {
+        let plugin_split = match_web_plugins(input).or_else(|| {
+            self.plugin_matchers
+                .inner
+                .values()
+                .map(|matcher| matcher.try_match(input))
+                .find_map(|match_| match_)
+        });
+
+        if let Some(query_data) = plugin_split {
+            self.input_value.modifier_display = query_data.modifier.clone();
+            self.input_value.mode = ActiveMode::from(query_data.clone());
+            // If plugin-hint is disabled use the full input,
+            // otherwise use the split value
+            self.input_value.input_display = if THEME.plugin_hint().is_none() {
+                input.to_string()
+            } else {
+                query_data.query
+            };
+        } else {
+            self.input_value.input_display = input.to_string();
+
+            if input.is_empty() {
+                self.input_value.mode = ActiveMode::History
+            } else {
+                self.input_value.mode = ActiveMode::DesktopEntry
+            }
+        }
+    }
+
+    fn set_input_with_modifier(&mut self, input: &str, previous_modi: String) {
+        if input.is_empty() {
+            self.input_value.modifier_display = "".to_string();
+            self.input_value.input_display = if THEME.plugin_hint().is_none() {
+                input.to_string()
+            } else {
+                previous_modi
+            };
+            self.input_value.mode = ActiveMode::DesktopEntry;
+            let _: iced::Command<Message> = text_input::move_cursor_to_end(INPUT_ID.clone());
+        } else {
+            self.input_value.input_display = input.to_string();
+        }
+    }
 }
 
 #[derive(Debug, Default)]
@@ -147,7 +223,7 @@ impl Default for State<'_> {
             scroll: Default::default(),
             input_value: SearchInput::default(),
             exec_on_next_search: false,
-            plugin_matchers: PluginConfigCache::default(),
+            plugin_matchers: PluginConfigCache::load(),
         }
     }
 }
