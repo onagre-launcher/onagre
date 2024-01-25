41d51191214a81a0afc6c9a91fd6bcc8715383cf
Paul Delafosse <paul.delafosse@protonmail.com>
2024-01-25 10:23:01 +0100

    chore: remaplace structopts with clap

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::path::PathBuf;
 
 use anyhow::anyhow;
 use app::style::Theme;
+use clap::Parser;
 use log::{info, LevelFilter};
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
-use structopt::StructOpt;
 use systemd_journal_logger::JournalLog;
 
 pub mod app;
@@ -25,14 +25,12 @@ pub static THEME_PATH: Lazy<Mutex<PathBuf>> = Lazy::new(|| {
 
 pub static THEME: Lazy<Theme> = Lazy::new(Theme::load);
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 #[structopt(name = "onagre", author = "Paul D. <paul.delafosse@protonmail.com>")]
 struct Cli {
-    #[structopt(
-        name = "theme",
-        short = "t",
+    #[arg(
         long = "theme",
-        takes_value = true,
+        short = 't',
         help = "Path to an alternate onagre theme file"
     )]
     theme: Option<PathBuf>,
@@ -42,7 +40,7 @@ pub fn main() -> iced::Result {
     JournalLog::new().unwrap().install().unwrap();
     log::set_max_level(LevelFilter::Info);
     info!("Starting onagre");
-    let cli = Cli::from_args();
+    let cli = Cli::parse();
     // User defined theme config, $XDG_CONFIG_DIR/onagre/theme.toml otherwise
     if let Some(theme_path) = cli.theme {
         let path = theme_path.canonicalize();
